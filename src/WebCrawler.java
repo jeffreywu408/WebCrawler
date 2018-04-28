8167cd0a679ed9fb1f50e453967d1b01e0a15b6a
Jeffrey <jeffreywu408@gmail.com>
2018-04-27 21:15:18 -0700

    Formatting

--- src/WebCrawler.java
+++ src/WebCrawler.java
@@ -35,267 +35,267 @@ public class WebCrawler {
     private static int id;
 
     public static boolean isRobotSafe(URL url) {
-	String robots = url.getProtocol() + "://" + url.getHost() + "/robots.txt";
-	String path = url.getFile();
+        String robots = url.getProtocol() + "://" + url.getHost() + "/robots.txt";
+        String path = url.getFile();
 
-	try {
-	    // Read the robots.txt line by line
-	    BufferedReader br = new BufferedReader(new InputStreamReader(new URL(robots).openStream()));
-	    String line;
+        try {
+            // Read the robots.txt line by line
+            BufferedReader br = new BufferedReader(new InputStreamReader(new URL(robots).openStream()));
+            String line;
 
-	    while ((line = br.readLine()) != null) {
-		String robotAgent = line.replaceAll("[ \\-:]", "").toLowerCase();
+            while ((line = br.readLine()) != null) {
+                String robotAgent = line.replaceAll("[ \\-:]", "").toLowerCase();
 
-		if (robotAgent.startsWith("useragent")) {
-		    robotAgent = robotAgent.substring("useragent".length()).toLowerCase().replace("*", ".*");
-		}
+                if (robotAgent.startsWith("useragent")) {
+                    robotAgent = robotAgent.substring("useragent".length()).toLowerCase().replace("*", ".*");
+                }
 
-		if (Pattern.matches(robotAgent, userAgent)) {
-		    while ((line = br.readLine()) != null && !line.trim().isEmpty()) {
-			String[] temp = line.toLowerCase().trim().split(":");
+                if (Pattern.matches(robotAgent, userAgent)) {
+                    while ((line = br.readLine()) != null && !line.trim().isEmpty()) {
+                        String[] temp = line.toLowerCase().trim().split(":");
 
-			if (temp[0].trim().equalsIgnoreCase("allow")) {
-			    if (temp.length > 1) {
-				// Case sensitive
-				// temp[1] = temp[1].trim().replace("*", ".*");
+                        if (temp[0].trim().equalsIgnoreCase("allow")) {
+                            if (temp.length > 1) {
+                                // Case sensitive
+                                // temp[1] = temp[1].trim().replace("*", ".*");
 
-				// Case insensitive
-				temp[1] = temp[1].trim().toLowerCase().replace("*", ".*");
-				temp[1] = temp[1].replace("?", "\\?");
+                                // Case insensitive
+                                temp[1] = temp[1].trim().toLowerCase().replace("*", ".*");
+                                temp[1] = temp[1].replace("?", "\\?");
 
-				if (temp[1].endsWith("$")) {
-				    temp[1] = temp[1].substring(0, temp[1].length());
-				    Pattern p = Pattern.compile(temp[1]);
-				    Matcher m = p.matcher(path);
+                                if (temp[1].endsWith("$")) {
+                                    temp[1] = temp[1].substring(0, temp[1].length());
+                                    Pattern p = Pattern.compile(temp[1]);
+                                    Matcher m = p.matcher(path);
 
-				    if (m.matches())
-					return true;
+                                    if (m.matches())
+                                        return true;
 
-				} else {
-				    temp[1] = temp[1].substring(0, temp[1].length());
-				    Pattern p = Pattern.compile(temp[1]);
-				    Matcher m = p.matcher(path);
+                                } else {
+                                    temp[1] = temp[1].substring(0, temp[1].length());
+                                    Pattern p = Pattern.compile(temp[1]);
+                                    Matcher m = p.matcher(path);
 
-				    if (m.lookingAt())
-					return true;
-				}
+                                    if (m.lookingAt())
+                                        return true;
+                                }
 
-			    } else {
-				// Nothing following "Allow:"
-				return false;
-			    }
-			}
+                            } else {
+                                // Nothing following "Allow:"
+                                return false;
+                            }
+                        }
 
-			if (temp[0].trim().equalsIgnoreCase("disallow") || temp[0].trim().equalsIgnoreCase("NoIndex")) {
-			    if (temp.length > 1) {
-				// Case sensitive
-				// temp[1] = temp[1].trim().replace("*", ".*");
+                        if (temp[0].trim().equalsIgnoreCase("disallow") || temp[0].trim().equalsIgnoreCase("NoIndex")) {
+                            if (temp.length > 1) {
+                                // Case sensitive
+                                // temp[1] = temp[1].trim().replace("*", ".*");
 
-				// Case insensitive
-				temp[1] = temp[1].trim().toLowerCase().replace("*", ".*");
-				temp[1] = temp[1].replace("?", "\\?");
+                                // Case insensitive
+                                temp[1] = temp[1].trim().toLowerCase().replace("*", ".*");
+                                temp[1] = temp[1].replace("?", "\\?");
 
-				if (temp[1].endsWith("$")) {
-				    temp[1] = temp[1].substring(0, temp[1].length());
-				    Pattern p = Pattern.compile(temp[1]);
-				    Matcher m = p.matcher(path);
+                                if (temp[1].endsWith("$")) {
+                                    temp[1] = temp[1].substring(0, temp[1].length());
+                                    Pattern p = Pattern.compile(temp[1]);
+                                    Matcher m = p.matcher(path);
 
-				    if (m.matches())
-					return false;
+                                    if (m.matches())
+                                        return false;
 
-				} else {
-				    temp[1] = temp[1].substring(0, temp[1].length());
-				    Pattern p = Pattern.compile(temp[1]);
-				    Matcher m = p.matcher(path);
+                                } else {
+                                    temp[1] = temp[1].substring(0, temp[1].length());
+                                    Pattern p = Pattern.compile(temp[1]);
+                                    Matcher m = p.matcher(path);
 
-				    if (m.lookingAt())
-					return false;
-				}
+                                    if (m.lookingAt())
+                                        return false;
+                                }
 
-			    } else {
-				// Nothing following "Disallow:"
-				return true;
-			    }
-			}
-		    }
-		}
-	    }
-	} catch (IOException | PatternSyntaxException e) {
-	    // System.out.println(e);
-	    return true;
-	}
+                            } else {
+                                // Nothing following "Disallow:"
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (IOException | PatternSyntaxException e) {
+            // System.out.println(e);
+            return true;
+        }
 
-	return true;
+        return true;
     }
 
     public static void appendReport(Response resp, String url, String report, String repository, int totalOutboundLinks,
-	    int outboundLinks) {
-	try {
-	    Document doc = resp.parse();
-	    int statusCode = resp.statusCode();
+            int outboundLinks) {
+        try {
+            Document doc = resp.parse();
+            int statusCode = resp.statusCode();
 
-	    FileWriter fw = new FileWriter(report, true);
-	    try (BufferedWriter out = new BufferedWriter(fw)) {
-		// Link to the live URL
-		out.write("<p><div><a href = " + url + ">" + doc.title() + "</a> (" + url + ")</div>");
+            FileWriter fw = new FileWriter(report, true);
+            try (BufferedWriter out = new BufferedWriter(fw)) {
+                // Link to the live URL
+                out.write("<p><div><a href = " + url + ">" + doc.title() + "</a> (" + url + ")</div>");
 
-		// Link to the downloaded page in the repository
-		String fileLocation = "file:///" + System.getProperty("user.dir") + "/" + repository + "/" + (id - 1)
-			+ ".html";
-		out.write("<div><a href = " + fileLocation + ">Repository Link</a></div>");
+                // Link to the downloaded page in the repository
+                String fileLocation = "file:///" + System.getProperty("user.dir") + "/" + repository + "/" + (id - 1)
+                        + ".html";
+                out.write("<div><a href = " + fileLocation + ">Repository Link</a></div>");
 
-		// Page statistics: HTTP status code, number of out-links, number of images
-		out.write("<div>HTTP Status Code: " + statusCode + "</div>");
-		out.write("<div>Outbound Links: " + totalOutboundLinks + "</div>");
-		out.write("<div>Outbound Links (domain restricted): " + outboundLinks + "</div>");
+                // Page statistics: HTTP status code, number of out-links, number of images
+                out.write("<div>HTTP Status Code: " + statusCode + "</div>");
+                out.write("<div>Outbound Links: " + totalOutboundLinks + "</div>");
+                out.write("<div>Outbound Links (domain restricted): " + outboundLinks + "</div>");
 
-		Elements images = doc.getElementsByTag("img");
-		out.write("<div>Number of Images: " + images.size() + "</div>");
+                Elements images = doc.getElementsByTag("img");
+                out.write("<div>Number of Images: " + images.size() + "</div>");
 
-		out.write("</p>");
-	    }
-	} catch (IOException e) {
-	    System.out.println(e);
-	}
+                out.write("</p>");
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 
     public static void crawlPage(String url, int numberLinks, String domain, String repository, String report) {
-	try {
-	    if (!isRobotSafe(new URL(url))) {
-		System.out.println("This page is not allowed to be crawled: " + url);
-		return;
-	    }
+        try {
+            if (!isRobotSafe(new URL(url))) {
+                System.out.println("This page is not allowed to be crawled: " + url);
+                return;
+            }
 
-	    // Get Document
-	    Response response = Jsoup.connect(url).execute();
-	    Document doc = response.parse();
+            // Get Document
+            Response response = Jsoup.connect(url).execute();
+            Document doc = response.parse();
 
-	    // Save into a repository
-	    File f = new File(repository + "/" + (id++) + ".html");
-	    try (BufferedWriter htmlWriter = new BufferedWriter(
-		    new OutputStreamWriter(new FileOutputStream(f), "UTF-8"))) {
-		htmlWriter.write(doc.toString());
-		htmlWriter.close();
-	    }
+            // Save into a repository
+            File f = new File(repository + "/" + (id++) + ".html");
+            try (BufferedWriter htmlWriter = new BufferedWriter(
+                    new OutputStreamWriter(new FileOutputStream(f), "UTF-8"))) {
+                htmlWriter.write(doc.toString());
+                htmlWriter.close();
+            }
 
-	    // Add current page to set of visited pages
-	    visitedPages.add(url);
-	    System.out.println(url);
+            // Add current page to set of visited pages
+            visitedPages.add(url);
+            System.out.println(url);
 
-	    // Get all links and add them to the queue
-	    Elements questions = doc.select("a[href]");
-	    int totalOutboundLinks = questions.size(); // Total number of outbound links
-	    int outboundLinks = 0; // Counter of outbound links restricted to the provided domain
+            // Get all links and add them to the queue
+            Elements questions = doc.select("a[href]");
+            int totalOutboundLinks = questions.size(); // Total number of outbound links
+            int outboundLinks = 0; // Counter of outbound links restricted to the provided domain
 
-	    for (Element link : questions) {
-		if (link.attr("abs:href").contains(domain)) {
-		    queue.add(link);
-		    outboundLinks++;
-		}
-	    }
+            for (Element link : questions) {
+                if (link.attr("abs:href").contains(domain)) {
+                    queue.add(link);
+                    outboundLinks++;
+                }
+            }
 
-	    // Update report
-	    appendReport(response, url, report, repository, totalOutboundLinks, outboundLinks);
+            // Update report
+            appendReport(response, url, report, repository, totalOutboundLinks, outboundLinks);
 
-	    // Visit page in queue
-	    while (!queue.isEmpty()) {
+            // Visit page in queue
+            while (!queue.isEmpty()) {
 
-		// Limit on how many pages to visit
-		if (visitedPages.size() >= numberLinks)
-		    return;
+                // Limit on how many pages to visit
+                if (visitedPages.size() >= numberLinks)
+                    return;
 
-		// For politeness, wait a moment before visiting link
-		TimeUnit.MILLISECONDS.sleep(WAIT);
+                // For politeness, wait a moment before visiting link
+                TimeUnit.MILLISECONDS.sleep(WAIT);
 
-		// Visit page (if not yet visited)
-		String link = queue.remove().attr("abs:href");
-		if (link.contains("#"))
-		    link = link.substring(0, link.indexOf("#"));
+                // Visit page (if not yet visited)
+                String link = queue.remove().attr("abs:href");
+                if (link.contains("#"))
+                    link = link.substring(0, link.indexOf("#"));
 
-		if (!visitedPages.contains(link)) {
-		    crawlPage(link, numberLinks, domain, repository, report);
-		}
-	    }
+                if (!visitedPages.contains(link)) {
+                    crawlPage(link, numberLinks, domain, repository, report);
+                }
+            }
 
-	} catch (org.jsoup.HttpStatusException e) {
-	    // Dealing with the HTTP Status Exception (such as 404)
-	    System.out.println(e);
-	    BufferedWriter out;
-	    try {
-		// Update report
-		out = new BufferedWriter(new FileWriter(report, true));
+        } catch (org.jsoup.HttpStatusException e) {
+            // Dealing with the HTTP Status Exception (such as 404)
+            System.out.println(e);
+            BufferedWriter out;
+            try {
+                // Update report
+                out = new BufferedWriter(new FileWriter(report, true));
 
-		out.write("<p><div><a href = " + url + ">" + url + "</a> (" + url + ")</div>");
-		out.write("<div>HTTP Status Code: " + e.getStatusCode() + "</div>");
-		out.write("</p>");
+                out.write("<p><div><a href = " + url + ">" + url + "</a> (" + url + ")</div>");
+                out.write("<div>HTTP Status Code: " + e.getStatusCode() + "</div>");
+                out.write("</p>");
 
-		out.close();
+                out.close();
 
-	    } catch (IOException ex) {
-		System.out.println(ex);
-	    }
+            } catch (IOException ex) {
+                System.out.println(ex);
+            }
 
-	} catch (MalformedURLException e) {
-	    System.out.println(e);
-	} catch (IOException | InterruptedException e) {
-	    System.out.println(e);
-	}
+        } catch (MalformedURLException e) {
+            System.out.println(e);
+        } catch (IOException | InterruptedException e) {
+            System.out.println(e);
+        }
     }
 
     public static void crawl(String fileName, String repository, String report) {
-	// Initialization
-	String url = "http://stackexchange.com";
-	String domain = "";
-	int maxPages = 100;
+        // Initialization
+        String url = "http://stackexchange.com";
+        String domain = "";
+        int maxPages = 100;
 
-	visitedPages = new HashSet<>();
-	queue = new LinkedList<>();
-	id = 0;
+        visitedPages = new HashSet<>();
+        queue = new LinkedList<>();
+        id = 0;
 
-	// Read in specifications
-	try (Scanner scanner = new Scanner(new File(fileName))) {
-	    scanner.useDelimiter(",");
+        // Read in specifications
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            scanner.useDelimiter(",");
 
-	    if (scanner.hasNext())
-		url = scanner.next();
-	    if (scanner.hasNext())
-		maxPages = Integer.parseInt(scanner.next());
-	    if (scanner.hasNext())
-		domain = scanner.next();
+            if (scanner.hasNext())
+                url = scanner.next();
+            if (scanner.hasNext())
+                maxPages = Integer.parseInt(scanner.next());
+            if (scanner.hasNext())
+                domain = scanner.next();
 
-	} catch (FileNotFoundException e) {
-	    System.out.println(e);
-	}
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
 
-	System.out.println("Seed: " + url);
-	System.out.println("Max number of pages: " + maxPages);
-	System.out.println("Domain Restriction: " + domain + "\n");
+        System.out.println("Seed: " + url);
+        System.out.println("Max number of pages: " + maxPages);
+        System.out.println("Domain Restriction: " + domain + "\n");
 
-	// Repository for storing downloaded pages
-	new File(repository).mkdirs();
+        // Repository for storing downloaded pages
+        new File(repository).mkdirs();
 
-	// Initialize report
-	try (PrintWriter writer = new PrintWriter(report, "UTF-8")) {
-	    writer.println("<p>Web Crawler</p>");
-	    writer.println("<div>Seed: <a href=" + url + ">" + url + "</a></div>");
-	    writer.println("<div>Max number of pages: " + maxPages + "</div>");
-	    writer.println("<div>Domain Restriction: " + domain + "</div>");
+        // Initialize report
+        try (PrintWriter writer = new PrintWriter(report, "UTF-8")) {
+            writer.println("<p>Web Crawler</p>");
+            writer.println("<div>Seed: <a href=" + url + ">" + url + "</a></div>");
+            writer.println("<div>Max number of pages: " + maxPages + "</div>");
+            writer.println("<div>Domain Restriction: " + domain + "</div>");
 
-	} catch (FileNotFoundException | UnsupportedEncodingException e) {
-	    System.out.println(e);
-	}
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            System.out.println(e);
+        }
 
-	// Start crawling
-	crawlPage(url, maxPages, domain, repository, report);
+        // Start crawling
+        crawlPage(url, maxPages, domain, repository, report);
     }
 
     public static void main(String[] args) {
-	try {
-	    crawl("specification.csv", "repository", "report.html");
-	    // ContentProcessor.processRepository("repository");
-	    // ContentProcessor.processFrequency("repository");
-	} catch (Exception e) {
-	    System.out.println(e);
-	}
+        try {
+            crawl("specification.csv", "repository", "report.html");
+            // ContentProcessor.processRepository("repository");
+            // ContentProcessor.processFrequency("repository");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
